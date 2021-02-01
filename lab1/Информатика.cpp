9e07150e7d09cb9d32fb13eba3438b4abee5784f
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-01 14:12:14 +0300

    Add files via upload

---
+++ lab1/Информатика.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+#include <cmath>
+int main()
+{
+    setlocale(0, "");
+    cout << "Введите номер упражнения" << endl;
+    int num;
+    cin >> num;
+    if (num == 1)
+    {
+        cout << "Введите катеты" << endl;
+        float a, b, c;
+        cin >> a;
+        cin >> b;
+        c = pow(pow(a, 2) + pow(b, 2), 0.5);
+        cout << "Гипотенуза = " << c << endl;
+    }
+    if (num != 1)
+    {
+        int N, i, j;
+        cout << "Введите N" << endl;
+        cin >> N;
+        for (j = 1; j <= N; j++)
+        {
+            if (num == 2)
+            {
+                for (i = 1; i <= N; i++)
+                {
+                    cout << "*";
+                }
+                cout << endl;
+            }
+            if (num == 3)
+            {
+                for (i = 1; i <= N; i++)
+                    if (i <= j)
+                    {
+                        cout << "*";
+                    }
+                cout << endl;
+            }
+            if (num == 4)
+            {
+                for (i = 1; i <= N; i++)
+                    if (i >= j)
+                    {
+                        cout << "*";
+                    }
+                cout << endl;
+            }
+            if (num == 5)
+            {
+                for (i = 1; i <= N - j + 1; i++)
+                    if (i < j)
+                    {
+                        cout << " ";
+                    }
+                    else
+                    {
+                        cout << "*";
+                    }
+                cout << endl;
+            }
+        }
+    }
+}
