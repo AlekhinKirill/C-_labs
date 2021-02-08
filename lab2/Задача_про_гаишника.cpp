c59bc07b99d3460f744c671e5af02d4a7a5ccaa9
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-08 20:17:48 +0300

    Add files via upload

---
+++ lab2/Задача_про_гаишника.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int speed, salary;
+	string identificator;
+	salary = 0;
+	do
+	{
+		cin >> speed >> identificator;
+		if ((speed > 60) && (identificator != "A999AA"))
+		{
+			if ((identificator[1] == identificator[2]) && (identificator[2] == identificator[3]))
+			{
+				salary = salary + 1000;
+			}
+			else if ((identificator[1] == identificator[2]) || (identificator[2] == identificator[3]) || (identificator[1] == identificator[3]))
+			{
+				salary = salary + 500;
+			}
+			else
+			{
+				salary = salary + 100;
+			}
+		}
+	} 
+	while (identificator != "A999AA");
+	cout << salary << endl;
+}
