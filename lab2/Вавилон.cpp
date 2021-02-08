e4583840690e2215199b3e4563a4e85fae4e46fd
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-08 21:59:31 +0300

    Add files via upload

---
+++ lab2/Вавилон.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number, degree, base, factor, i, j;
+	base = 1;
+	degree = 0;
+	cin >> number;
+    while (number >= base*60)
+	{
+		base = base * 60;
+		degree = degree + 1;
+	}
+	for (i = degree; i >= 0; i--)
+	{
+		factor = number / base;
+		while (factor / 10 != 0)
+		{
+			cout << "<";
+			factor = factor - 10;
+		}
+		for (j = 1; j <= factor; j++)
+		{
+			cout << "v";
+		}
+		if (i != 0)
+		{
+			cout << ".";
+		}
+		number = number % base;
+		base = base / 60;
+	}
+}
