f7bf4d955686d2fd432fdee22686dd88e932a40d
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-08 13:49:10 +0300

    Add files via upload

---
+++ lab2/НОД.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int num, denum, intermediate, first_number, second_number;
+	cout << "Введите натуральные числа" << endl;
+	cin >> first_number >> second_number;
+	if (first_number >= second_number)
+	{
+		num = first_number;
+		denum = second_number;
+	}
+	else
+		denum = first_number;
+	    num = second_number;
+	while (denum != 0)
+	{
+		intermediate = denum;
+		denum = num % denum;
+		num = intermediate;
+	}
+	cout << num << endl;
+}
