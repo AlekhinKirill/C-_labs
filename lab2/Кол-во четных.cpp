afbf38cd17273bbf39b89b9edd9c75af3101a472
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-08 14:50:54 +0300

    Add files via upload

---
+++ lab2/Кол-во четных.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int element, num;
+	num = -1;
+	do
+	{
+		cin >> element;
+		if (element % 2 == 0)
+		{
+			num = num + 1;
+		}
+	}
+	while (element != 0);
+	cout << num << endl;
+}
