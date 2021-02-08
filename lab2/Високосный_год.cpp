49df2dd11a8137b6b448c0a52816e863b421a3c2
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-08 14:38:16 +0300

    Add files via upload

---
+++ lab2/Високосный_год.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int year;
+	cin >> year;
+	if (((year % 100 == 0) && (year / 100 != 4)) || (year % 4 != 0))
+	{
+		cout << "NO" << endl;
+	}
+	else
+	{
+		cout << "Yes" << endl;
+	}
+}
