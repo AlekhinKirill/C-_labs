e8c75249a4a04e7a491df95e9802d38ad9dec2c3
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-08 14:09:42 +0300

    Add files via upload

---
+++ lab2/НОД.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, denum, intermediate, first_number, second_number;
+	cin >> first_number >> second_number;
+	if (first_number >= second_number)
+	{
+		num = first_number;
+		denum = second_number;
+	}
+	else
+	{
+		denum = first_number;
+		num = second_number;
+	}
+	while (denum != 0)
+	{
+		intermediate = denum;
+		denum = num % denum;
+		num = intermediate;
+	}
+	cout << num << endl;
+}
