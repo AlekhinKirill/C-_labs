80456d93a5f03ec64774d22e22314e18b7ea6d45
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-15 12:03:46 +0300

    Add files via upload

---
+++ lab2/Степень двойки.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+	cin >> number;
+	while (number % 2 == 0)
+	{
+		number = number / 2;
+	}
+	if (number == 1)
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+}
