d52be0c76e3c0a66682bac7bae5ea2a8685b74a3
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-15 13:34:51 +0300

    Add files via upload

---
+++ lab3/Задача 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, i, Answer;
+	float mean, Sum_1;
+	Sum_1 = 0;
+	Answer = 0;
+	cin >> N;
+	int* arr = new int[N];
+	for (i = 0; i <= N - 1; i++)
+	{
+		cin >> arr[i];
+		Sum_1 += arr[i];
+	}
+	mean = Sum_1 / N;
+	for (i = 0; i <= N - 1; i++)
+	{
+		if (arr[i] > mean)
+		{
+			Answer += arr[i];
+		}
+	}
+	cout << Answer << endl;
+}
