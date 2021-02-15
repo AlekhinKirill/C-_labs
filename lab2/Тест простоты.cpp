e738f74590d153b3b1f351c15fcd72dbed183a2b
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-15 12:10:34 +0300

    Add files via upload

---
+++ lab2/Тест простоты.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number, i, key;
+	cin >> number;
+	key = 1;
+	for (i = 2; i <= number - 1; i++)
+	{
+		if (number % i == 0)
+		{
+			key = 0;
+		}
+	}
+	cout << key << endl;
+}
