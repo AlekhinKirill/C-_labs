3c02abdf939fc4c7da91e8c97c13a167342b781d
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-15 12:08:08 +0300

    Add files via upload

---
+++ lab2/Факторизация.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number, max;
+	max = 0;
+	do
+	{
+		cin >> number;
+		if ((number % 2 == 0) && (number > max))
+		{
+			max = number;
+		}
+	} while (number != 0);
+	cout << max << endl;
+}
