f5c57590f30f84518449065179455efb66c724c1
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-02-23 12:01:39 +0300

    Add files via upload

---
+++ Lists/ConsoleApplication1.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Cells
+{
+	int value;
+	Cells* next_ptr;
+};
+
+struct List
+{
+	int length;
+	Cells* first;
+	Cells* last;
+};
+
+List create_empty()
+{
+	List list;
+	list.length = 0;
+	list.first = nullptr;
+	list.last = nullptr;
+	return list;
+}
+
+List append(List list, int value)
+{
+	Cells* cell = new Cells;
+	(*cell).value = value;
+	(*cell).next_ptr = nullptr;
+	if (list.length == 0)
+	{
+		list.first = cell;
+	}
+	else
+	{
+		if (list.length == 1)
+		{
+			list.last = list.first;
+			(*list.first).next_ptr = cell;
+		}
+		(*list.last).next_ptr = cell;
+		list.last = cell;
+	}
+	list.length += 1;
+	return list;
+}
+
+void print_list(List list)
+{
+	if (list.length == 0)
+		cout << "Лист пуст" << endl;
+	else
+	{
+		Cells last_cell;
+		last_cell = *list.first;
+		while (last_cell.next_ptr != nullptr)
+		{
+			cout << last_cell.value << endl;
+			last_cell = *last_cell.next_ptr;
+		}
+		cout << last_cell.value << endl;
+	}
+}
+
+List remove(List list, int a)
+{
+	List new_list;
+	Cells *cell, *previous_cell;
+	int warning;
+	warning = 0;
+	if (list.length >= 2)
+	{
+		cell = (*list.first).next_ptr;
+		previous_cell = list.first;
+		new_list.first = list.first;
+		new_list.length = list.length - 1;
+		while ((*cell).value != a)
+		{
+			if ((*cell).next_ptr != nullptr)
+			{
+				previous_cell = cell;
+				cell = (*cell).next_ptr;
+			}
+			else
+			{
+				cout << "Элемента " << a << " в списке нет!" << endl;
+				warning = 1;
+				break;
+			}
+		}
+		if (warning == 0)
+		{
+			(*previous_cell).next_ptr = (*cell).next_ptr;
+			return new_list;
+		}
+		else
+			return list;
+	}
+	else
+	{
+		if (list.length == 0)
+			return list;
+		else
+		{
+			if ((*list.first).value == a)
+				return create_empty();
+			else
+			{
+				cout << "Элемента " << a << " в списке нет!" << endl;
+				return list;
+			}
+		}
+	}
+}
+
+int main()
+{
+	setlocale(0, "");
+	List list;
+	list = create_empty();
+	list = append(list, 9);
+	list = remove(list, 9);
+	print_list(list);
+}
