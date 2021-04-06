7e505452c39049af5704e8303195c72421ad4536
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2021-04-06 22:47:11 +0300

    Add files via upload

--- queues_&_stacks/Queue.cpp
+++ queues_&_stacks/Queue.cpp
@@ -2,294 +2,88 @@
 using namespace std;
 
 
-struct Cells
+struct Cell
 {
 	int value;
-	Cells* next_ptr;
+	Cell* next_ptr;
 };
 
-struct List
+struct Queue
 {
 	int length;
-	Cells* first;
-	Cells* last;
+	Cell* first;
+	Cell* last;
 };
 
-List create_empty()
+
+Queue create_empty()
 {
-	List list;
+	Queue list;
 	list.length = 0;
 	list.first = nullptr;
 	list.last = nullptr;
 	return list;
 }
 
-List push(List list, int value)
+
+Queue enqueue(Queue queue, int value)
 {
-	Cells* cell = new Cells;
-	(*cell).value = value;
-	(*cell).next_ptr = nullptr;
-	if (list.length == 0)
+	Cell* cell = new Cell;
+	cell->value = value;
+	cell->next_ptr = nullptr;
+	if (queue.length == 0)
 	{
-		list.first = cell;
+		queue.first = cell;
 	}
 	else
 	{
-		if (list.length == 1)
+		if (queue.length == 1)
 		{
-			list.last = list.first;
-			(*list.first).next_ptr = cell;
+			queue.last = queue.first;
+			queue.first->next_ptr = cell;
 		}
-		(*list.last).next_ptr = cell;
-		list.last = cell;
+		queue.last->next_ptr = cell;
+		queue.last = cell;
 	}
-	list.length += 1;
-	return list;
+	queue.length += 1;
+	return queue;
 }
 
-void print_list(List list)
+
+Queue dequeue(Queue queue)
 {
-	if (list.length == 0)
-		cout << "Лист пуст" << endl;
-	else
-	{
-		Cells last_cell;
-		last_cell = *list.first;
-		while (last_cell.next_ptr != nullptr)
-		{
-			cout << last_cell.value << " ";
-			last_cell = *last_cell.next_ptr;
-		}
-		cout << last_cell.value << endl;
-	}
+	queue.first = queue.first->next_ptr;
+	queue.length -= 1;
+	return queue;
 }
 
-List remove(List list, int a)
+
+void print(Queue queue)
 {
-	List new_list;
-	Cells *cell, *previous_cell;
-	int warning;
-	warning = 0;
-	if (list.length >= 2)
+	Cell* element_ptr;
+	element_ptr = queue.first;
+	cout << queue.first->value << " ";
+	while (element_ptr->next_ptr != nullptr)
 	{
-		if ((*list.first).value == a)
-		{
-			new_list.first = (*list.first).next_ptr;
-			new_list.length = list.length - 1;
-			new_list.last = list.last;
-			return new_list;
-		}
-		else
-		{
-			cell = (*list.first).next_ptr;
-			previous_cell = list.first;
-			new_list.first = list.first;
-			new_list.length = list.length - 1;
-			new_list.last = list.last;
-			while ((*cell).value != a)
-			{
-				if ((*cell).next_ptr != nullptr)
-				{
-					previous_cell = cell;
-					cell = (*cell).next_ptr;
-				}
-				else
-				{
-					cout << "Элемента " << a << " в списке нет!" << endl;
-					warning = 1;
-					break;
-				}
-			}
-			if (warning == 0)
-			{
-				(*previous_cell).next_ptr = (*cell).next_ptr;
-				if ((*cell).next_ptr == nullptr)
-					new_list.last = previous_cell;
-				return new_list;
-			}
-			else
-				return list;
-		}
+		cout << element_ptr->next_ptr->value << " ";
+		element_ptr = element_ptr->next_ptr;
 	}
-	else
-	{
-		if (list.length == 0)
-			return list;
-		else
-		{
-			if ((*list.first).value == a)
-				return create_empty();
-			else
-			{
-				cout << "Элемента " << a << " в списке нет!" << endl;
-				return list;
-			}
-		}
-	}
+	cout << endl;
 }
-
-List merge(List first_list, List second_list)
-{
-	Cells* cell;
-	if (second_list.length != 0)
-	{
-		cell = second_list.first;
-		while ((*cell).next_ptr != nullptr)
-		{
-			first_list = push(first_list, (*cell).value);
-			cell = (*cell).next_ptr;
-		}
-		first_list = push(first_list, (*cell).value);
-	}
-	return first_list;
-}
-
-
-List add_i(List list, int value, int i)
-{
-	List new_list;
-	Cells* cell;
-	Cells* new_cell = new Cells;
-	int j;
-	if (i == 1)
-	{
-		new_list = create_empty();
-		new_list.length = list.length + 1;
-		(*new_cell).value = value;
-		(*new_cell).next_ptr = list.first;
-		new_list.first = new_cell;
-		new_list.last = list.last;
-		return new_list;
-	}
-	else if ((list.length != 0) && (list.length >= i))
-	{
-		new_list = create_empty();
-		new_list.length = list.length + 1;
-		cell = list.first;
-		for (j = 1; j < i - 1; j++)
-		{
-			cell = (*cell).next_ptr;
-		}
-		(*new_cell).value = value;
-		(*new_cell).next_ptr = (*cell).next_ptr;
-		(*cell).next_ptr = new_cell;
-		new_list.first = list.first;
-		new_list.last = list.last;
-		return new_list;
-	}
-	else
-	{
-		cout << "В списке нет "<< i << "-ого элемента" << endl;
-		return list;
-	}
-}
-
-List del_i(List list, int i)
-{
-	List new_list;
-	Cells* cell;
-	Cells* previous_cell = new Cells;
-	if (i == 1)
-	{
-		return remove(list, (*list.first).value);
-	}
-	else
-	{
-		int j;
-		new_list = create_empty();
-		new_list.length = list.length - 1;
-		cell = list.first;
-		for (j = 1; j < i; j++)
-		{
-			previous_cell = cell;
-			cell = (*cell).next_ptr;
-		}
-		(*previous_cell).next_ptr = (*cell).next_ptr;
-		new_list.first = list.first;
-		new_list.last = list.last;
-		return new_list;
-	}
-}
-
-void reverse(List list)
-{
-	int i, j;
-	Cells* cell = new Cells;
-	for (i = 0; i < list.length; i++)
-	{
-		cell = list.first;
-		for (j = 1; j < list.length - i; j++)
-		{
-			cell = (*cell).next_ptr;
-		}
-		cout << (*cell).value << endl;
-	}
-}
-
-int index(List list, int value)
-{
-	int i;
-	Cells* cell;
-	cell = list.first;
-	for (i = 1; i <= list.length; i++)
-		if ((*cell).value == value)
-		{
-			return i;
-			break;
-		}
-		else if ((*cell).next_ptr != nullptr)
-		{
-			cell = (*cell).next_ptr;
-		}
-		else
-		{
-			cout << "В списке нет элемента " << value << endl;
-			return NULL;
-			break;
-		}
-}
-
-List copy_list(List list)
-{
-	Cells* cell;
-	List copy;
-	copy = create_empty();
-	cell = list.first;
-	while ((*cell).next_ptr != nullptr)
-	{
-		copy = push(copy, (*cell).value);
-		cell = (*cell).next_ptr;
-	}
-	copy = push(copy, (*cell).value);
-	return copy;
-}
-
-List dequeue(List list)
-{
-	list = del_i(list, list.length);
-	return list;
-}
-
-List enqueue(List queue, int element)
-{
-	return add_i(queue, element, 1);
-}
-
 
 
 int main()
 {
-	setlocale(0, "");
-	List queue;
+	Queue queue;
 	queue = create_empty();
 	queue = enqueue(queue, 5);
-	queue = enqueue(queue, 7);
-	queue = enqueue(queue, 3);
-	queue = enqueue(queue, 9);
-	queue = enqueue(queue, 8);
-	queue = dequeue(queue);
-	queue = dequeue(queue);
+	queue = enqueue(queue, 12);
+	queue = enqueue(queue, 17);
 	queue = enqueue(queue, 1);
-	cout << "Очередь:" << endl;
-	print_list(queue);
+	queue = dequeue(queue);
+	queue = enqueue(queue, 8);
+	queue = enqueue(queue, 11);
+	queue = enqueue(queue, 7);
+	queue = dequeue(queue);
+	print(queue);
 }
